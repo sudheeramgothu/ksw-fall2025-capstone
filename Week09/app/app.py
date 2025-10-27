61df2f4b922e7ed0296fe765ded3005a9a396e9a
sudheeramgothu <sudheer.amgoth3@gmail.com>
2025-10-27 10:06:14 -0400

    Update app.py

--- Week09/app/app.py
+++ Week09/app/app.py
@@ -10,3 +10,21 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+------
+After deployment, visit:
+http://<app-service-ip>:5000/metrics
+You should see metrics like request count, latency, and response codes.
+
+
+
+Verify Metrics Collection:
+
+Check in Prometheus:
+
+kubectl port-forward svc/prometheus-kube-prometheus-prometheus -n monitoring 9090:9090
+
+
+Then go to: http://localhost:9090
+ → Status → Targets
+You should see your flask-app endpoint listed as UP.
